da88bdbcf72781f93264d74f354ca334e098b976
wataru <wataru.oguchi.0to1@gmail.com>
2025-05-08 05:13:33 +0000

    init - shell

---
+++ apps/frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+const rootDom = document.getElementById("root");
+if (!rootDom) {
+	throw new Error("Failed to find root element");
+}
+const root = createRoot(rootDom);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
